0580a20b7f9ab782042456897612cabc11072414
leon12082003 <markus.bringer@gmx.de>
2025-06-24 13:36:45 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ WORK_HOURS = {
     "fri": ["08:00", "18:00"]
 }
 
-SERVICE_ACCOUNT_FILE = "fitnessstudio-sonnenberg-86e2cdf96c.json"
-CALENDAR_ID = "primary"
+SERVICE_ACCOUNT_FILE = "service-account.json"
+CALENDAR_ID = "ab771cebfa8568bc7bf235ebffe22cca74458429c19dce4590e01a7bd1b07182@group.calendar.google.com"
 
 SLOT_DURATION_MINUTES = 30
